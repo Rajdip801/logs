9172654405d646927b82e08fa18b034a537be315
Nero <jstadude69@gmail.com>
2025-05-25 03:09:55 +0530

    Initial commit

--- src/react-app/pages/test.tsx
+++ src/react-app/pages/test.tsx
@@ -1,14 +1,10 @@
 // src/Login.js
 
 const Test = () => {
-  const apiUrl = import.meta.env.VITE_API_URL;
-  console.log('API URL:', apiUrl);
-
   // Your login form logic here
   return (
     <div>
-      <h1>Login Page</h1>
-      <p>API URL: {apiUrl}</p>
+<h1>Test Page</h1>
     </div>
   );
 };
